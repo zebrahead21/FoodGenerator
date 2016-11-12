39ed52d056f57bf546ace7db65194c6e007e778f
Camelia Moise <camelia.moise21@gmail.com>
2016-11-12 15:35:53 +0200

    Rails App

---
+++ FoodGeneratorApp/config/application.rb
@@ -0,0 +1,43 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+ 
+module FoodGeneratorApp
+	class Application < Rails::Application
+    config.recipes = Elasticsearch::Persistence::Repository.new do
+		  client Elasticsearch::Client.new url: 'http://localhost:9200', log: true
+		  index :recipes
+		  type  :data_record
+		end
+
+		config.ingredients = Elasticsearch::Persistence::Repository.new do
+			client Elasticsearch::Client.new url: 'http://localhost:9200', log: true
+			index :ingredients
+			type  :data_record
+		end
+
+		config.relationships_recipe_ingredients = Elasticsearch::Persistence::Repository.new do
+		  client Elasticsearch::Client.new url: 'http://localhost:9200', log: true
+		  index :relationships_recipe_ingredients
+		  type  :data_record
+		end
+	end
+end
+
+
+class DataRecord
+  attr_reader :attributes
+
+  def initialize(attributes={})
+    @attributes = attributes
+  end
+
+  def to_hash
+    @attributes
+  end
+end
