2934ca40b4b904e14e1f4f6bd1068fb497c6e2dc
Dan Serban <dserban01@yahoo.com>
2016-11-13 14:09:08 +0200

    Cosmetic changes to Angular app

--- FoodGeneratorApp/public/js/app.js
+++ FoodGeneratorApp/public/js/app.js
@@ -4,25 +4,16 @@ var app = angular.module('AngularToDoListApp', []);
 
 app.controller('AngularToDoListController', ['$scope', '$http', function($scope, $http) {
 
-	function posting(ingredient_name) {
-		$http.post('/putting_ingredient', {'ingredient_name':ingredient_name})
-			 .success(function(data, status, headers, config) {
-				$scope.ingredients = data;
-
-      });
-	}
-	$scope.posting = posting; 
-
-
-
-
-
-
-
-
-
+  function posting(ingredient_name) {
+    $http.post('/putting_ingredient', { 'ingredient_name': ingredient_name })
+         .success(function(data, status, headers, config) {
+           $scope.ingredients = data;
+         });
+  }
+  $scope.posting = posting; 
 
 
 
 
 }]);
+
