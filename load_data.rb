59713ff0f0345da402940f4d3fd7bc7e66dfa064
Camelia Moise <camelia.moise21@gmail.com>
2016-11-19 08:58:56 +0200

    fixing load_data.rb for the new input format. Looks good on kibana too

--- load_data.rb
+++ load_data.rb
@@ -39,39 +39,29 @@ relationships.create_index! force: true
 
 hashmap = {}  
 
-# hashmap cu cheie reteta si valorea lista de ingrediente folosite, despartite printr-un '|'
-# are buguri momentan din cauza inputului
-# e citita in indexul de retete
-# folosita pentru cautarea dupa mai multe ingrediente
 
 CSV.read('database/relationships.csv',
-  {:col_sep => '|'}).map { |row| hashmap[row[3].split('#')[0]] ||= []
-                                 hashmap[row[3].split('#')[0]] << row[3].split('#')[1] 
+  {:col_sep => '|'}).map { |row| hashmap[row[2].split('#')[0]] ||= []
+                                 hashmap[row[2].split('#')[0]] << row[2].split('#')[1] 
                                  relationships.save({ 
-                                        'id': Digest::SHA256.hexdigest(row[3]),
-                                        'ingredient_id': row[3].split('#')[1], 
-                                        'recipe_id': row[3].split('#')[0], 
-                                        'quantity': row[0], 
-                                        'unit_measure': row[1],
-                                        'semi_preparation': row[4],
-                                        'structured_ingredient': row[2] })} 
-puts
-puts
-puts hashmap
-puts
-puts
+                                        'id': Digest::SHA256.hexdigest(row[2]),
+                                        'ingredient_id': row[2].split('#')[1], 
+                                        'recipe_id': row[2].split('#')[0], 
+                                        'ingredient_details': row[1],
+                                        'structured_ingredient': row[0] })} 
+
 
 CSV.read('database/recipes.csv',
   {:col_sep => '|'}).map { |row| recipes.save({ 
                                         'id': row[0],
                                         'all_ingredients': hashmap[row[0]].join('|'),
-                                        'descr': row[1], 
+                                        'recipe_name': row[1], 
                                         'preparation': row[2] }) }
 
 CSV.read('database/ingredients.csv',
   {:col_sep => '|'}).map { |row| ingredients.save({ 
                                         'id': row[1], 
                                         'url_key': row[0], 
-                                        'description': row[2] }) }
+                                        'ingredient_name': row[2] }) }
 
 
