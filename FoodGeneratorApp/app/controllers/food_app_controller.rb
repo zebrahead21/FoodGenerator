989570b3a80cbbbb3a01718a4c63f492f0646045
iulian <iulianbarbu2@gmail.com>
2016-11-20 11:41:50 +0200

    Added interface for choosing ingredients

--- FoodGeneratorApp/app/controllers/food_app_controller.rb
+++ FoodGeneratorApp/app/controllers/food_app_controller.rb
@@ -1,14 +1,26 @@
 class FoodAppController < ActionController::Base
-	# @@recipes = Rails.configuration.recipes
-	# @@relationships = Rails.configuration.relationships
-	
 
+	@@recipes = Rails.configuration.recipes
+	@@relationships = Rails.configuration.relationships
+	@@ingredients = Rails.configuration.ingredients	
+		
+
+
+
+
+	def retriveIngredients()
+		ingredients = @@ingredients.search('ingredient_name:' + params[:ingredientHolder]).to_a.map(&:to_hash)	
+		render :json => ingredients
+	end	
+	
 
 	def putting_ingredient
 		session[:ingredients] ||= []
 		session[:ingredients] << params[:ingredient_name]
+		puts
+		puts session[:ingredients]
+		puts
 		render :json => session[:ingredients]
 	end
-
 
 end
--- FoodGeneratorApp/config/routes.rb
+++ FoodGeneratorApp/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # get '/main_page' => 'food_app#main_page'
-  get '/ingredient' => 'food_app#ingredient'
-  post '/putting_ingredient' => 'food_app#putting_ingredient'
-  
+  get '/main_page' => 'food_app#main_page'
+  get '/ingredientsThatMatchIngredientHolder.json' => 'food_app#retriveIngredients'
 
+  post '/putting_ingredient' => 'food_app#putting_ingredient'
 
 end
