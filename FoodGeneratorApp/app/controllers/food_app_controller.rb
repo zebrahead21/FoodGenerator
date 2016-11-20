c7480d0b56cfd980e41696ec55bf118342156af2
vladimir29 <iordachev29@gmail.com>
2016-11-20 14:54:52 +0200

    viewing recipes

--- FoodGeneratorApp/app/controllers/food_app_controller.rb
+++ FoodGeneratorApp/app/controllers/food_app_controller.rb
@@ -7,37 +7,19 @@ class FoodAppController < ActionController::Base
 
 
 
+	@@recipes_found = []
+	@ingredients_prep = {}
+	@visible_recipe = 0
+	@relationships
+
 
 	def retriveIngredients()
 		ingredients = @@ingredients.search('ingredient_name:' + params[:ingredientHolder]).to_a.map(&:to_hash)	
 		render :json => ingredients
 	end	
 	
-
-	def putting_ingredient
-		session[:ingredients] ||= []
-		session[:ingredients] << params[:ingredient_name]
-		puts
-		puts session[:ingredients]
-		puts
-		render :json => session[:ingredients]
-	end
-
-
 	def search_recipes
-
 		ingredients = params[:ingredients_list]
-		# puts
-		# puts
-		# puts
-		# puts
-		# puts "da"
-		# puts ingredients
-		# puts
-		# puts
-		# puts
-		# puts
-		# puts
 		ingredients_string = "all_ingredients:("
 		ingredients.each do |i|
 			ingredients_string << i.to_s.gsub('+', '_').gsub('-', '_')
@@ -47,18 +29,21 @@ class FoodAppController < ActionController::Base
 		ingredients_string = ingredients_string[0..ingredients_string.length - 6]
 		ingredients_string << ")"
 		puts ingredients_string
-		recipes_found = @@recipes.search(ingredients_string).to_a.map(&:to_hash)
-		# puts
-		# puts
-		# puts
-		# puts
-		# puts "nu"
-		# puts recipes_found
-		# puts
-		# puts
-		# puts
-		# puts 
-		render :json => recipes_found
+		@@recipes_found = @@recipes.search(ingredients_string).to_a.map(&:to_hash)
+	
+		render :json => @@recipes_found
+	end
+
+	def recipeDetailedView
+		@@recipes_found.each do | recipe |
+			if recipe['id'] == params[:id]
+				@visible_recipe = recipe
+				break
+			end
+		end
+
+		@relationships = @@relationships.search('recipe_id:' + params[:id]).to_a.map(&:to_hash)	
+		render 'recipe'
 	end
 
 end
--- FoodGeneratorApp/config/routes.rb
+++ FoodGeneratorApp/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/main_page' => 'food_app#main_page'
   get '/ingredientsThatMatchIngredientHolder.json' => 'food_app#retriveIngredients'
+  get '/recipe/:id' => 'food_app#recipeDetailedView'
 
   post '/getIngredientsList' => 'food_app#search_recipes'
-  
 
 end
